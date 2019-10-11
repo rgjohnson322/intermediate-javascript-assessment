436b8dabccf9b076f4f0686de35d0c1a15f66fcf
rgjohnson322 <rgjohnson322@gmail.com>
2019-10-11 15:44:48 -0500

    part one kinda done

--- part1/assessment-part1.js
+++ part1/assessment-part1.js
@@ -7,20 +7,20 @@
 
 // Given the following nested functions:
 
-function daBears(){
+function daBears() {
   var isFurry = true;
 
-  function papaBear (){
+  function papaBear() {
     var porridge = "Too Hot!";
     var chair = "Too Big!";
     var bed = "Too Hard!";
     var feeling = "Angry";
 
-    function mamaBear(){
+    function mamaBear() {
       var porridge = "Too Cold!";
       var bed = "Too Soft!";
 
-      function babyBear(){
+      function babyBear() {
         var porridge = "Just right!";
         var chair = "Just right!";
         var bed = "Just right!";
@@ -30,7 +30,7 @@ function daBears(){
     }
   }
 
-  function goldilocks(){
+  function goldilocks() {
     var feeling = "Hungry";
     var isFurry = false;
     var isDinner = true;
@@ -43,27 +43,29 @@ function daBears(){
 // Which function(s) access the "chair" variable and get "Too Big!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale1 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale1 = ["papaBear", "mamaBear"];
+
+
 
 // Which function(s) access the "feeling" variable and get "Hungry"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale2 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale2 = ["goldilocks"];
 
 // Which function(s) access the "porridge" variable and get "Too Cold!"
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale3 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale3 = ["mamaBear"];
 
 // Which function(s) access the "sleepy" variable and get undefined
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale4 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale4 = ["daBears", "papaBear", "mamaBear", "goldilocks"];
 
 // Which function(s) access the isFurry variable and get true
 // (Delete wrong answers, leave correct ones)
 
-var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
+var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear"];
 
 
 // *************
@@ -83,7 +85,22 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // CODE HERE...
 
+function Vehicle() {
+  this.gasRemaining = 100;
 
+  this.drive = () => {
+    this.gasRemaining -= 25;
+    return this.gasRemaining
+  }
+};
+Vehicle.prototype.drive = () => {
+
+};
+var charger = new Vehicle(100);
+var mustang = new Vehicle(100);
+charger.drive();
+mustang.drive();
+mustang.drive();
 
 
 
@@ -101,13 +118,23 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 // For example: "OH HELLO THERE" becomes "Oh Hello There"
 
-// Your method may be passed punctuation, numbers or other non-letter characters
-// and should neither modify them nor break when encountering them.
+// Your method may bers or other non-letter characters
+// and should neither modify them nor break whe passed punctuation, numben encountering them.
 
 
 
 
 // CODE HERE...
+String.prototype.grammarPolice = (phrase) => {
+
+  let wordsArray = phrase.toLowerCase().split(/\s+/);
+  let upperCased = wordsArray.map(function (word) {
+    return word.charAt(0).toUpperCase() + word.substr(1);
+  });
+  return upperCased.join(" ");
+};
+String("strangely-capitalized strings that offend the internet")
+
 
 
 
@@ -126,7 +153,15 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 // In all other cases, return "Different values"
 
 // CODE HERE...
-
+function valueType(one, two) {
+  if (one === two) {
+    return "Exactly the same";
+  } else if (one == two) {
+    return "Same value, different types";
+  } else {
+    return "Different values";
+  }
+}
 
 
 // *************
@@ -140,4 +175,10 @@ var fairyTale5 = ["daBears", "papaBear", "mamaBear", "babyBear", "goldilocks"];
 
 var theAnswer = "Unknown";
 
+
 // CODE HERE...
+function promiseCatcher(para) {
+  para.then(function(result) {
+    theAnswer = result;
+  });
+}
